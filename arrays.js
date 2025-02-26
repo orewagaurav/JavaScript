731e9139b9bdadc710d8eac95e40ee1c578c3a27
orewagaurav <gauravkumar47967@gmail.com>
2025-02-26 17:49:37 +0530

    conditionals

---
+++ arrays.js
@@ -0,0 +1,7 @@
+console.log("Introduction to the arrays in JS");
+let arr=[1,2,3,4,5,6,7,8,9];
+console.log(arr);
+console.log(arr.length);
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[5]);
