5a3b20ccd885abe8be6f3d9c9cf397ebfdfe19af
orewagaurav <gauravkumar47967@gmail.com>
2025-02-26 17:58:04 +0530

    arrays

--- arrays.js
+++ arrays.js
@@ -1,7 +1,21 @@
 console.log("Introduction to the arrays in JS");
+
 let arr=[1,2,3,4,5,6,7,8,9];
 console.log(arr);
 console.log(arr.length);
 console.log(arr[0]);
 console.log(arr[1]);
 console.log(arr[5]);
+
+//arrays are mutable
+arr[0]=0;
+console.log(arr);
+arr[1]="Gaurav";
+console.log(arr);
+console.log(typeof(arr));
+
+console.log(arr.toString());
+console.log(arr.join(" and "));
+console.log(arr.join("  "));
+// console.log(arr.toString());
+
