fb2ba2c4b79fbcc8e99a9afd813ae6e3daf3f159
orewagaurav <gauravkumar47967@gmail.com>
2025-02-28 00:10:27 +0530

    Dom & Bom

---
+++ dom.js
@@ -0,0 +1,8 @@
+console.log("Hello Dom");
+
+// document.childNode[1].childNodes;
+// let cont =document.childNode[1].childNodes;
+// cont
+// cont.firstChild;
+// cont.lastChild;
+//document.childNodes[1].firstElementChild;
