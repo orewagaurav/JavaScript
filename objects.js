02871df8afa9cfc928c4e8d84a776035aa883616
orewagaurav <gauravkumar47967@gmail.com>
2025-04-18 17:31:09 +0530

    objects

---
+++ objects.js
@@ -0,0 +1,21 @@
+//object literals
+
+const mySym = symbol("key1");
+
+
+const jsUser = {
+    name:"Gaurav",
+    "known AS":"orewagaurav",
+    age: 20,
+    location: "Noida",
+    email:"orewagaurav1@gmail.com",
+    isLoggedIn:false,
+    lastLoginDate: ["Monday","Saturday"]
+}
+console.log(jsUser.name);
+console.log(jsUser["email"]); 
+console.log(jsUser["known AS"]); 
+
+
+ 
+
