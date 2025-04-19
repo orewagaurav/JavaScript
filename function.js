f8c09433c3a0916c93726e768cf13f97c7e874b7
orewagaurav <gauravkumar47967@gmail.com>
2025-04-19 10:42:00 +0530

    f

--- function.js
+++ function.js
@@ -18,3 +18,36 @@
 // arrow(34);
 
 
+function add(x,y){
+    let z = x+y;
+    return z; 
+}
+// console.log(add("gaurav",4));
+
+function loginUserMessage(username="orewagaurav"){
+    if(!username){
+        return "plz enter username first..." 
+    }
+    return `${username} just logged in...`;
+}
+  
+let f = loginUserMessage("hmlo");
+// console.log(f);
+
+function calculateCartPrice(...num){
+    return num;
+}
+console.log(calculateCartPrice(23,12,45,67));
+
+const user = {
+    name:"Gaurav",
+    age:20,
+    location:"Noida",
+    email:"orewagaurav@gmail.com"
+}
+
+function handelObject(anyObject){
+    if(!anyObject) return -1;
+    return `User's name is ${anyObject.name} and his age is ${anyObject.age}`
+}
+console.log(handelObject(user));
