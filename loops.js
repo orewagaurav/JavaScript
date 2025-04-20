d3b2dac22b3b12183a8435e58d960de217f40a26
orewagaurav <gauravkumar47967@gmail.com>
2025-04-20 14:50:28 +0530

    loops

--- loops.js
+++ loops.js
@@ -1,46 +1,46 @@
-console.log("Introduction to the Loops in JS");
-//type of loops in js
+// console.log("Introduction to the Loops in JS");
+// //type of loops in js
 
-// 1.for loop
-// 2.while loop
-// 3.do-while loop
-// 3.for in loop
-// 4.for of loop
+// // 1.for loop
+// // 2.while loop
+// // 3.do-while loop
+// // 3.for in loop
+// // 4.for of loop
 
-let a=1;
+// let a=1;
 
-for(let i=0;i<50;i++){
-    console.log(a+i);
-}
-//for in loop
-let obj={
-    name:"Gaurav",
-    roll:23,
-    studentId:2301331720023,
-    school:"ABPS"
-}
-for(let key in obj){
-    let value =obj[key];
-    console.log(key,value);
-}
+// for(let i=0;i<50;i++){
+//     console.log(a+i);
+// }
+// //for in loop
+// let obj={
+//     name:"Gaurav",
+//     roll:23,
+//     studentId:2301331720023,
+//     school:"ABPS"
+// }
+// for(let key in obj){
+//     let value =obj[key];
+//     console.log(key,value);
+// }
 
-//for of loop
-for (const c of "Gaurav Kumar") {
-    console.log(c);
-}
+// //for of loop
+// for (const c of "Gaurav Kumar") {
+//     console.log(c);
+// }
 
-//while loop
-let wil=0;
-while(wil<18){
-    console.log(wil);
-    wil++;
-}
+// //while loop
+// let wil=0;
+// while(wil<18){
+//     console.log(wil);
+//     wil++;
+// }
 
-//do while 
-let d=1;
-let e=2;
-do {
-    console.log("its false");
-    d++;
-} while (d==e);
+// //do while 
+// let d=1;
+// let e=2;
+// do {
+//     console.log("its false");
+//     d++;
+// } while (d==e);
 
