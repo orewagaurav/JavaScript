8cb2ff4d861f5c8af403590797110ea7ac013209
orewagaurav <gauravkumar47967@gmail.com>
2025-04-23 23:52:23 +0530

    p

---
+++ project1/script1.js
@@ -0,0 +1,12 @@
+const buttons = document.querySelectorAll(".button")
+const body = document.querySelector("body");
+
+buttons.forEach(function (button){
+    console.log(button);
+    button.addEventListener("click",function(e){
+        console.log(e.target);
+        if(e.target === "grey"){
+            body.style.backgroundColor = e.target.id; 
+        }
+    })
+});
