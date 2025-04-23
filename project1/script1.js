27aa2e516bcbc7b133c4e39568a946bd22db41d6
orewagaurav <gauravkumar47967@gmail.com>
2025-04-24 00:07:30 +0530

    project-1

--- project1/script1.js
+++ project1/script1.js
@@ -2,11 +2,42 @@ const buttons = document.querySelectorAll(".button")
 const body = document.querySelector("body");
 
 buttons.forEach(function (button){
-    console.log(button);
     button.addEventListener("click",function(e){
-        console.log(e.target);
         if(e.target.id === "grey"){
-            body.style.backgroundColor = e.target.id; 
+            body.style.backgroundColor = e.target.id;
+            body.style.color = "red";
+        }
+        if(e.target.id === "white"){
+            body.style.backgroundColor = e.target.id;
+            body.style.color = "green";
+        }
+        if(e.target.id === "blue"){
+            body.style.backgroundColor = e.target.id;
+            body.style.color = "pink";
+        }
+        if(e.target.id === "yellow"){
+            body.style.backgroundColor = e.target.id;
+            body.style.color = "purple";
         }
     })
-});
+}); 
+
+//pro version
+
+// const colorMap = {
+//     grey: "red",
+//     white: "green",
+//     blue: "pink",
+//     yellow: "purple"
+// };
+
+// buttons.forEach(function (button) {
+//     button.addEventListener("click", function (e) {
+//         const bgColor = e.target.id;
+//         const textColor = colorMap[bgColor];
+//         if (textColor) {
+//             body.style.backgroundColor = bgColor;
+//             body.style.color = textColor;
+//         }
+//     });
+// });
