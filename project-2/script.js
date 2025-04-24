009a9190425d7073f985cbec4ca70d4cf378871b
orewagaurav <gauravkumar47967@gmail.com>
2025-04-24 08:38:28 +0530

    project-2

--- project-2/script.js
+++ project-2/script.js
@@ -6,6 +6,7 @@ form.addEventListener("submit",function(e){
     const height = parseInt(document.querySelector("#height").value) 
     const weight = parseInt(document.querySelector("#weight").value)
     const result = document.querySelector("#results")
+    const comment =document.querySelector("#comment")
     
     if(height==='' || height<0 || isNaN(height)){
         result.innerHTML = `please enter a valid height ${height}`
@@ -14,7 +15,14 @@ form.addEventListener("submit",function(e){
         result.innerHTML = `please enter a valid weight ${weight}`
     }
     else{
-        const bmi = (weight / ((height*height)/10000)). toFixed (2)
+        const bmi = (weight / ((height*height)/10000)). toFixed (2);
         result.innerHTML = `<span>${bmi}</span>`;
+        if(bmi < 18.6){
+            comment.innerHTML = "Under Weight";
+        } else if(bmi >= 18.6 && bmi <= 24.9){
+            comment.innerHTML = "Normal Weight";
+        } else {
+            comment.innerHTML = "Over Weight";
+        }
     }
 }); 
