bd5fc39b2dda88ee88267c68d09a1455ad794522
orewagaurav <gauravkumar47967@gmail.com>
2025-04-26 01:03:44 +0530

    promises

--- advance_js/promises.js
+++ advance_js/promises.js
@@ -4,6 +4,38 @@ const promiseOne = new Promise(function(resolve,reject){
 
     setTimeout(function(){
         console.log("Async task is complete");
+        resolve()
         
     },1000)
+})
+
+promiseOne.then(function(){
+    console.log("Promise consumed");
+})
+
+//Promise 2
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log("Async task 2");
+        resolve();
+    },1000)
+}).then(function(){
+    console.log("Async 2 resolved");
+})
+
+//promise 3
+const promiseThree = new Promise(function(resolve,reject){
+    setTimeout(() => {
+        resolve({
+            userName:"orewagaurav",
+            name:"Gaurav",
+            age:20,
+            email:"orewagaurav1@gmail.com"
+        })
+        
+    }, 1000);
+})
+
+promiseThree.then(function(user){
+    console.log(user.name);
 })
