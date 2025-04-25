c6cc98997323c40e269ccdd2ccf3f390afb1c18f
orewagaurav <gauravkumar47967@gmail.com>
2025-04-25 15:14:51 +0530

    p

--- project-5/script.js
+++ project-5/script.js
@@ -4,4 +4,14 @@ const randomColor  = function(){
     for(let i= 0;i<6;i++){ //q ki hex color code 6 digit ka hota hai
         color += hex[Math.floor(Math.random()*16)];
     }
+    return color;
 }
+const startChangingColor = function(){
+    
+}
+const stopChangingColor = function(){
+
+}
+
+document.querySelector("#start").addEventListener("click",startChangingColor);
+document.querySelector("#stop").addEventListener("click",stopChangingColor);
